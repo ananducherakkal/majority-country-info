51c6007e13ff33aadfd83fb51222275d82bf6ba4
ananducherakkal <ananducherakkal.dev@gmail.com>
2024-06-05 13:57:41 +0200

    feat: initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: "CountryInfo",
+  meta: [
+    { name: "country info", content: "To get information about countries." },
+  ],
+});
+</script>
+
+<style lang="scss" scoped></style>
